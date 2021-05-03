a35e242369a7210e07b0ad86b3d15f78132f94dd
Lucas Jaroszewski <nunes.lucasj@gmail.com>
2021-05-03 16:45:14 -0300

    Fixed test_phonerecharges.py

--- test_phonerecharges.py
+++ test_phonerecharges.py
@@ -3,7 +3,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "PhoneRecharges/1", {'company_id': 'claro_11', 'product_id': 'claro_10', 'value': 10.0, 'phone_number': 5511999999999})
+response = requests.put(BASE + "PhoneRecharges/1", {'company_id': 'claro_12', 'product_id': 'claro_10', 'value': 10.0, 'phone_number': 5511999999999})
 print('POST /PhoneRecharges/1')
 print(response.json())
 print('')
@@ -18,7 +18,7 @@ print('POST /PhoneRecharges/2 - 409: produto nao existe')
 print(response.json())
 print('')
 
-response = requests.put(BASE + "PhoneRecharges/2", {'company_id': 'claro_11', 'product_id': 'claro_20', 'value': 20.0, 'phone_number': 5511999999999})
+response = requests.put(BASE + "PhoneRecharges/2", {'company_id': 'claro_12', 'product_id': 'claro_20', 'value': 20.0, 'phone_number': 5511999999999})
 print('POST /PhoneRecharges/2 - Nova recarga com mesmo numero')
 print(response.json())
 print('')
